4fc99f1c2655ab88cf09ff9d0aa4390915b3832f
Nicoleta Ciobanu <ciobanunicoleta25@gmail.com>
2021-06-23 21:30:33 +0800

    implement a simple Rails application with 2 pages

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,20 +1,20 @@
 class QuestionsController < ApplicationController
+
+  def ask; end
+
+
   def answer
     @question = params[:question]
-    @answer = coach_answer(@question)
-    @class = 'answer-green'
-  end
-
-  def ask
-  end
-  def coach_answer(question)
-    if question.downcase == "i am going to work right now!"
-      'GREAT!'
-    elsif question.end_with?("?")
-      "Silly question, get dressed and go to work!"
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
     else
-      "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
-  # check if
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-
-  get 'answer', to: 'questions#answer'
-  get 'ask', to: 'questions#ask'
+  root to: 'questions#ask'
+  get 'answer', to: 'questions#answer', as: 'answer'
+  get 'ask', to: 'questions#ask', as: 'ask'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
